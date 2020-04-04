551d02a294a2ebfb48f2b461a0961ab275015b35
mlnima <mmlnima@gmail.com>
2020-04-04 12:13:54 +0200

    sidebarIssueFixedForDeploy

---
+++ setupForFirstTime.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+require('dotenv').config()
+const settingSchema = require('./server/models/settings/settingSchema')
+const widgetSchema = require('./server/models/settings/widgetSchema')
+const postSchema = require('./server/models/postSchema')
+
+const mongoDBConnectionUrl = process.env.DB_LOCAL ?
+    `mongodb://localhost:${ process.env.DB_PORT }/${ process.env.DB_NAME }` :
+    `mongodb://${ process.env.DB_USER }:${ process.env.DB_PASS }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`
+
+mongoose.connect(mongoDBConnectionUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log('DB not connected', err));
+
+//identity default setting
+const identityData = {
+    type: 'identityTest',
+    data: {
+        "siteAddress": '',
+        "protocol": "http",
+        "title": "just another React CMS website",
+        "themeColor": "#000",
+        "description": "website description",
+        "keywords": [],
+        "logoText": "Logo text",
+        "headLine": "this is the site headline",
+        "homePageH1": "H1",
+        "homePagePagination": true,
+        "postsCountPerPage": "40",
+        "homePageSidebar": true,
+        "categoriesPageSidebar": true,
+        "tagsPageSidebar": true,
+        "actorsPageSidebar": true,
+        "postPageSidebar": true,
+        "postsPageSidebar": true
+    }
+}
+
+const runSetup = async () => {
+    const identityToSave = new settingSchema(identityData)
+    await identityToSave.save()
+}
+
+runSetup().then(() => {
+    console.log('all set')
+    process.exit()
+}).catch(err => {
+    console.log(err)
+    process.exit()
+})
