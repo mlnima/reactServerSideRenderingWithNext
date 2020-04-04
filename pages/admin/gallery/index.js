b9f2e1e1915f2d0f8c62f49ea3a6d03021b61611
mlnima <mmlnima@gmail.com>
2020-04-04 18:52:17 +0200

    sidebarIssueFixedForDeploy

---
+++ pages/admin/gallery/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState, useContext, useRef } from 'react';
+import AdminLayout from '../../../components/layouts/AdminLayout'
+
+const gallery = props => {
+    const [ state, setState ] = useState({});
+    useEffect(() => {
+    }, []);
+    return (
+        <AdminLayout>
+            <div>
+                under develop
+            </div>
+        </AdminLayout>
+    );
+};
+export default gallery;
