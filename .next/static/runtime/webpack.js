4480c674ad463c7f91d27950039d2a5e0d66801b
mlnima <mmlnima@gmail.com>
2020-09-04 22:42:21 +0200

    headerWidgetAreaDynamic

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c73d8e4120644087d5d0";
+/******/ 	var hotCurrentHash = "186f658fa8baa38067cc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/includes/Header/Header.js
+++ components/includes/Header/Header.js
@@ -1,26 +1,29 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { AppContext } from '../../../context/AppContext'
+import React, {useEffect, useState, useContext} from 'react';
+import {AppContext} from '../../../context/AppContext'
 import WidgetsRenderer from '../WidgetsRenderer/WidgetsRenderer'
 
-const Header = props => {
+const Header = () => {
     const contextData = useContext(AppContext);
-    const [ state, setState ] = useState({
-        style:{}
+    const [state, setState] = useState({
+        style: {},
     });
 
     useEffect(() => {
+        const justifyContentPropertyOfHeader = contextData.siteWidgets.filter(i=>i.data.position === 'header').length >1 ? 'space-between' : 'center'
         setState({
             ...state,
-            style:{
-                backgroundColor:contextData.siteDesign.headerBackgroundColor,
-                color:contextData.siteDesign.headerTextColor
+            style: {
+                ...state.style,
+                backgroundColor: contextData.siteDesign.headerBackgroundColor,
+                color: contextData.siteDesign.headerTextColor,
+                justifyContent: justifyContentPropertyOfHeader
             }
         })
-    }, [contextData.siteDesign]);
+    }, [contextData.siteDesign,contextData.siteWidgets]);
 
     return (
         <div className='header' style={state.style}>
-            <WidgetsRenderer widgets={ contextData.siteWidgets } position='header'/>
+            <WidgetsRenderer widgets={contextData.siteWidgets} position='header'/>
         </div>
     );
 };
