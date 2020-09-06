ce14d9a9eb58bfb4217901e447bf1ae6ffdc4b39
mlnima <mmlnima@gmail.com>
2020-09-06 14:39:23 +0200

    sideBarResponsiveBugFixed

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c5268fbfc34bdd1c9265";
+/******/ 	var hotCurrentHash = "319d78f20a9a8848b804";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
