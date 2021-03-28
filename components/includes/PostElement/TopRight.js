4049c27c994a8f00b8c632578c496f9e6d2dbf34
mlnima <mmlnima@gmail.com>
2021-03-28 22:15:21 +0200

    post quality updated

--- components/includes/PostElement/TopRight.js
+++ components/includes/PostElement/TopRight.js
@@ -5,7 +5,12 @@ const TopRight = props => {
 
     const quality =  props.quality === '2160p' ? '4K' :
                      props.quality === '1440p' ? '2K' :
-                     props.quality === '1080p' ? 'HD' : 'SD'
+                     props.quality === '1080p' ? 'HD' :
+                     props.quality === '720p' ? 'SD' :
+                     props.quality === '480p' ? 'SD' :
+                     props.quality === '360p' ? 'SD' :
+                     props.quality === '240p' ? 'SD' :
+                     props.quality
 
 
     return (
