4a168dd556660ccfa7b6fb265a3992b492e412af
mlnima <mmlnima@gmail.com>
2021-04-04 18:14:18 +0200

    footer bug fixed

--- _variables/ajaxVariables.js
+++ _variables/ajaxVariables.js
@@ -19,13 +19,13 @@ export const saveCustomStyle = async (data) => {
 };
 
 export const getSetting = async (type, domainName, cache, whichPage) => {
-    const pageNameForCachedRequest = whichPage ? `&position=${ whichPage }`: ''
+    const pageNameForCachedRequest = whichPage ? `&position=${whichPage}` : ''
     const body = {
         type,
         cache
     };
 
-    return await axios.post(domainName + `/api/v1/settings/get?type=${ type }${ pageNameForCachedRequest }`, body)
+    return await axios.post(domainName + `/api/v1/settings/get?type=${type}${pageNameForCachedRequest}`, body)
 };
 
 export const addNewWidget = async (data) => {
@@ -49,12 +49,12 @@ export const getSingleWidgetData = async (data) => {
 //     return await axios.post(domainName + '/api/v1/settings/getWidget', body)
 // }
 
-export const getMultipleWidgetWithData = async (widgets, domainName,cache, whichPage) => {
+export const getMultipleWidgetWithData = async (widgets, domainName, cache, whichPage) => {
     const body = {
         ...widgets,
         cache
     };
-    return await axios.post(domainName + `/api/v1/settings/getMultipleWidgetWithData?whichPage=${ whichPage }`, body)
+    return await axios.post(domainName + `/api/v1/settings/getMultipleWidgetWithData?whichPage=${whichPage}`, body)
 }
 
 export const getMultipleSetting = async (settings, domainName, cache, whichPage) => {
@@ -62,7 +62,7 @@ export const getMultipleSetting = async (settings, domainName, cache, whichPage)
         ...settings,
         cache
     };
-    return await axios.post(domainName + `/api/v1/settings/getMultiple?whichPage=${ whichPage }`, body)
+    return await axios.post(domainName + `/api/v1/settings/getMultiple?whichPage=${whichPage}`, body)
 };
 
 export const getWidgetsWithData = async (position, domainName) => {
@@ -121,7 +121,7 @@ export const userImageUpload = async (image) => {
 }
 // will be remove
 export const contactAjaxPost = async (data) => {
-    const body ={
+    const body = {
         data
     }
     return await axios.post(window.location.origin + '/api/v1/form/contact', body)
@@ -129,20 +129,20 @@ export const contactAjaxPost = async (data) => {
 //-- forms
 
 export const saveFormWidgetData = async (data) => {
-    const body ={
+    const body = {
         data
     }
     return await axios.post(window.location.origin + '/api/v1/forms/save', body)
 }
 
 export const getFormData = async (data) => {
-    const body ={
+    const body = {
         data
     }
     return await axios.post(window.location.origin + '/api/v1/forms/get', body)
 }
-export const getSingleFormData = async (data,domainName) => {
-    const body ={
+export const getSingleFormData = async (data, domainName) => {
+    const body = {
         ...data
     }
     return await axios.post(domainName + '/api/v1/forms/getFormData', body)
@@ -150,33 +150,33 @@ export const getSingleFormData = async (data,domainName) => {
 
 //pages
 export const saveNewPage = async (data) => {
-    const body ={
+    const body = {
         ...data
     }
     return await axios.post(window.location.origin + '/api/v1/pages/new', body)
 }
 export const updatePage = async (data) => {
-    const body ={
+    const body = {
         ...data,
         token: localStorage.wt
     }
     return await axios.post(window.location.origin + '/api/v1/pages/update', body)
 }
-export const getPageData = async (data,domainName) => {
-    const body ={
+export const getPageData = async (data, domainName) => {
+    const body = {
         ...data
     }
     return await axios.post(domainName + '/api/v1/pages/getPageData', body)
 }
-export const deletePage = async (id,domainName) => {
-    const body ={
+export const deletePage = async (id, domainName) => {
+    const body = {
         id,
         token: localStorage.wt
     }
     return await axios.post(domainName + '/api/v1/pages/deletePage', body)
 }
 export const getPagesData = async (data) => {
-    const body ={
+    const body = {
         ...data,
         token: localStorage.wt
     }
@@ -202,23 +202,23 @@ export const getOrders = async (data, domainName) => {
     return await axios.post(domainName + `/api/v1/order/get`, body)
 };
 
-export const getFirstLoadData = async  req =>{
+export const getFirstLoadData = async req => {
     const domainName = req ? await getAbsolutePath(req) : '';
     const refererUrl = req?.headers?.referer
-    console.log(refererUrl)
-    const referer = refererUrl ? refererUrl.includes(req?.headers?.host)  :false
+    //console.log(refererUrl)
+    const referer = refererUrl ? refererUrl.includes(req?.headers?.host) : false
     //const referer = !!req.headers.referer
-    const isSameOrigin =  req.headers['sec-fetch-site'] === 'same-origin';
-    const isNavigatedFromPostPage = /video|post|article|product/.test(req.headers.referer)
-    const firstLoadWidgetsData = !req.headers.referer ? await getMultipleWidgetWithData({widgets: ['footer', 'header', 'topBar', 'navigation']}, domainName, true, 'firstLoadWidgetsData') : []
+    const isSameOrigin = req.headers['sec-fetch-site'] === 'same-origin';
+    const isNavigatedFromPostPage = /video|post|article|product/.test(refererUrl || '')
+    const firstLoadWidgetsData = referer ? await getMultipleWidgetWithData({widgets: ['footer', 'header', 'topBar', 'navigation']}, domainName, true, 'firstLoadWidgetsData') : []
     //const settingsData = await getMultipleSetting({settings: ['identity', 'design']}, domainName, true, 'postPage')
-    const settingsData =!req.headers.referer ? await getMultipleSetting({settings: ['identity', 'design']}, domainName, true, 'postPage'):{}
-    let isMobile = (req ? req.headers['user-agent']: navigator.userAgent).match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
+    const settingsData = referer ? await getMultipleSetting({settings: ['identity', 'design']}, domainName, true, 'postPage') : {}
+    let isMobile = (req ? req.headers['user-agent'] : navigator.userAgent).match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i)
 
     return {
-        domainName:req ? await getAbsolutePath(req) : '',
-        settings:settingsData?.data?.settings ?? [],
-        widgets:firstLoadWidgetsData?.data?.widgets ?? [],
+        domainName: req ? await getAbsolutePath(req) : '',
+        settings: settingsData?.data?.settings ?? [],
+        widgets: firstLoadWidgetsData?.data?.widgets ?? [],
         referer,
         isSameOrigin,
         isNavigatedFromPostPage,
