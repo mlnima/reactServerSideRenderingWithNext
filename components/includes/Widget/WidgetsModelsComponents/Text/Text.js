5e4ba2cd2b3f5c0aebee7fa37d376f7b3362e966
mlnima <mmlnima@gmail.com>
2021-05-09 22:00:25 +0200

    styled jsx improved

--- components/includes/Widget/WidgetsModelsComponents/Text/Text.js
+++ components/includes/Widget/WidgetsModelsComponents/Text/Text.js
@@ -1,10 +1,10 @@
 import React, {useContext, useRef} from 'react';
 import {AppContext} from "../../../../../context/AppContext";
 import parse from 'html-react-parser';
+const contextData = useContext(AppContext);
 
 const Text = props => {
     const spanElement = useRef(null)
-    const contextData = useContext(AppContext);
     const textData = props.translations ? props.translations[contextData.state.activeLanguage] ? props.translations[contextData.state.activeLanguage].text || props.text : props.text : props.text
     const data = parse(textData)
     return (
