9e0671f909cd596e61d0ce7a661f61310ba66c43
mlnima <mmlnima@gmail.com>
2021-05-16 19:18:05 +0200

    PWA dynamic icons

--- components/global/RedirecterToHttps/RedirecterToHttps.js
+++ components/global/RedirecterToHttps/RedirecterToHttps.js
@@ -34,7 +34,7 @@ const RedirecterToHttps = () => {
 
     if (state.render) {
         return (
-            <a ref={redirectElement} href={state.sslUrl.replace('http','https')} rel='noreferrer'>Redirect to HTTPS</a>
+            <a ref={redirectElement} href={state.sslUrl} rel='noreferrer'>Redirect to HTTPS</a>
         );
     } else return null
 
