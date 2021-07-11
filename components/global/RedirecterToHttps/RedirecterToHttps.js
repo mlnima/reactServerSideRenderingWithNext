0d234c6a6ac7fdb80a40b67eb01b1f4238aaac77
mlnima <mmlnima@gmail.com>
2021-07-11 17:33:12 +0200

    ios loading data bug updated

--- components/global/RedirecterToHttps/RedirecterToHttps.js
+++ components/global/RedirecterToHttps/RedirecterToHttps.js
@@ -39,7 +39,7 @@ const RedirecterToHttps = props => {
     }, [state.render]);
 
 
-    if (state.render && props.identity.autoRedirectToHttps ) {
+    if (state?.render && props?.identity?.autoRedirectToHttps ) {
         //console.log(state.render,props.identity.autoRedirectToHttps)
         return (
             <a ref={redirectElement} href={state.sslUrl} rel='noreferrer'>Redirect to HTTPS</a>
