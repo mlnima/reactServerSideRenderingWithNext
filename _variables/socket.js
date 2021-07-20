2a0dd27bc36bfe7009d2e75e0aa38328e7ee466e
mlnima <mmlnima@gmail.com>
2021-07-20 19:54:03 +0200

    video call added

--- _variables/socket.js
+++ _variables/socket.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client')
 const url = process.env.NODE_ENV !== 'production' ?
     process.env.REACT_APP_PRODUCTION_URL.replace(process.env.REACT_APP_PORT, parseInt(process.env.REACT_APP_PORT) + 1) :
-    process.env.REACT_APP_PRODUCTION_URL.replace(process.env.REACT_APP_PORT, parseInt(process.env.REACT_APP_PORT) + 1) + '/socket'
+    process.env.REACT_APP_PRODUCTION_URL.replace(process.env.REACT_APP_PORT, parseInt(process.env.REACT_APP_PORT) + 1)
 
 
 module.exports = io(url)
