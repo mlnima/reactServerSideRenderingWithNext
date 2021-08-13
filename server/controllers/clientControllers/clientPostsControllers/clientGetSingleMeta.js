297cc1911318e63ac141adc6c90eec7e6bdb6a2a
mlnima <mmlnima@gmail.com>
2021-08-13 13:35:27 +0200

    get single meta api updated

--- server/controllers/clientControllers/clientPostsControllers/clientGetSingleMeta.js
+++ server/controllers/clientControllers/clientPostsControllers/clientGetSingleMeta.js
@@ -1,8 +1,11 @@
 const metaSchema = require('../../../models/metaSchema');
+const mongoose = require('mongoose')
 
 
 module.exports = async (req, res) => {
-    metaSchema.findById(req.body.id).exec().then(meta => {
+    const isValid = mongoose.isValidObjectId(req.body.id)
+    const query = isValid ? {_id:req.body.id} : {name:req.body.id}
+    metaSchema.findOne(query).exec().then(meta => {
         res.json({meta})
         res.end()
     }).catch(err => {
