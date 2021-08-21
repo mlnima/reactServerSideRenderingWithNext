ec9b4545a15524ddc14940aebf6741b667396972
mlnima <mmlnima@gmail.com>
2021-08-21 17:43:43 +0200

    error page updated

--- pages/_error.js
+++ pages/_error.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 const Error = ({responseCode}) => {
 
+    console.log(responseCode)
     return (
         <div className='error-page'>
-            <h1 className='error-page-message'>
+            <style jsx>{`
+              #not-found-page {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 50vh;
+
+                h1 {
+                  color: var(--main-text-color);
+                }
+                .back-to-homepage{
+                  color: var(--main-text-color);
+                  text-decoration: none;
+                }
+              }
+            `}</style>
                 <h1 className='error-page-message'>
                     {responseCode
                         ? `error ${responseCode} occurred on server`
                         : 'An error occurred on client'}
                 </h1>
-            </h1>
         </div>
     )
 }
