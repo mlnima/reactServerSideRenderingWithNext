3a185c9cba38d0cd007e47cf243e2ddc5aa2a95e
mlnima <mmlnima@gmail.com>
2021-08-27 20:55:55 +0200

    text widget updated

--- components/includes/Widget/WidgetsModelsComponents/Text/Text.js
+++ components/includes/Widget/WidgetsModelsComponents/Text/Text.js
@@ -2,6 +2,14 @@ import React, {useContext, useRef} from 'react';
 import {AppContext} from "../../../../../context/AppContext";
 import parse from 'html-react-parser';
 
+import styled from "styled-components";
+const TextStyledDiv = styled.div`
+  color:var(--main-text-color);
+  max-width: 100vw;
+  @media only screen and (min-width: 768px) {
+    color:var(--main-text-color);
+  }
+`
 
 const Text = props => {
     const contextData = useContext(AppContext);
@@ -9,25 +17,9 @@ const Text = props => {
     const textData = props.translations ? props.translations[contextData.state.activeLanguage] ? props.translations[contextData.state.activeLanguage].text || props.text : props.text : props.text
     const data = parse(textData)
     return (
-        <div className='widgetText' ref={spanElement} >
-            <style jsx>{`
-                .widgetText{
-                color:var(--main-text-color);
-               // width:clamp(45ch,90%,75ch);
-                //text-align: center;
-                //margin: auto;
-                max-width: 100vw;
-                }
-            @media only screen and (min-width: 768px) {
-                .widgetText{
-                color:var(--main-text-color);
-                //width:clamp(45ch,50%,75ch);
-                }
-            }
-            `}</style>
+        <TextStyledDiv className='widgetText' ref={spanElement} >
             {data}
-
-        </div>
+        </TextStyledDiv>
     );
 };
 export default Text;
