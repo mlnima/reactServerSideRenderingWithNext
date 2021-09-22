4a1183ec683ad54e5e67d733cbe177f012feefbf
mlnima <mmlnima@gmail.com>
2021-09-23 01:34:25 +0200

    article type card bug fixed

--- pages/staticIndex.js
+++ pages/staticIndex.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { getFirstLoadDataStatic} from "../_variables/ajaxVariables";
-import MainWidgetArea from "../components/widgetsArea/MainWidgetArea/MainWidgetArea";
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
-const Home = ({isMobile, widgets, design, identity}) => {
-
-    return (
-        < MainWidgetArea isMobile={isMobile}
-                         rendering={true}
-                         widgets={(widgets || []).filter(widget => widget.data?.position === 'home')}
-                         className='home-page main'
-                         position='home'
-                         stylesData={design?.homePageStyle}
-                         currentPageSidebar={identity?.homePageSidebar }
-                         postElementSize={design?.postElementSize}
-                         postElementStyle={design?.postElementStyle}
-                         postElementImageLoader={design?.postElementImageLoader}
-                         postElementImageLoaderType={design?.postElementImageLoaderType}
-        />
-    );
-
-};
-
-export const getStaticProps = async (context) => {
-    const firstLoadData = await getFirstLoadDataStatic(context.req, ['homePageLeftSidebar', 'homePageRightSidebar', 'home'])
-    return {
-        props: {
-            ...(await serverSideTranslations(context.locale, ['common','customTranslation'])),
-            widgets : firstLoadData?.widgets || [],
-            ...firstLoadData?.settings,
-            isMobile: firstLoadData?.isMobile ? Boolean(firstLoadData.isMobile) :false,
-            referer: firstLoadData?.referer ? firstLoadData?.referer :false,
-        }}
-}
-
-// export default Home;
+// import React from 'react';
+// import { getFirstLoadDataStatic} from "../_variables/ajaxVariables";
+// import MainWidgetArea from "../components/widgetsArea/MainWidgetArea/MainWidgetArea";
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+//
+// const Home = ({isMobile, widgets, design, identity}) => {
+//
+//     return (
+//         < MainWidgetArea isMobile={isMobile}
+//                          rendering={true}
+//                          widgets={(widgets || []).filter(widget => widget.data?.position === 'home')}
+//                          className='home-page main'
+//                          position='home'
+//                          stylesData={design?.homePageStyle}
+//                          currentPageSidebar={identity?.homePageSidebar }
+//                          postElementSize={design?.postElementSize}
+//                          postElementStyle={design?.postElementStyle}
+//                          postElementImageLoader={design?.postElementImageLoader}
+//                          postElementImageLoaderType={design?.postElementImageLoaderType}
+//         />
+//     );
+//
+// };
+//
+// export const getStaticProps = async (context) => {
+//     const firstLoadData = await getFirstLoadDataStatic(context.req, ['homePageLeftSidebar', 'homePageRightSidebar', 'home'])
+//     return {
+//         props: {
+//             ...(await serverSideTranslations(context.locale, ['common','customTranslation'])),
+//             widgets : firstLoadData?.widgets || [],
+//             ...firstLoadData?.settings,
+//             isMobile: firstLoadData?.isMobile ? Boolean(firstLoadData.isMobile) :false,
+//             referer: firstLoadData?.referer ? firstLoadData?.referer :false,
+//         }}
+// }
+//
+// // export default Home;
