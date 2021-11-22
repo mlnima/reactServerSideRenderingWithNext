fc8985b6de881f9984c7afac60ec6ee5d3d6907e
mlnima <mmlnima@gmail.com>
2021-11-22 14:24:35 +0100

    auto deleted video detector updated

--- server/controllers/adminControllers/adminPostsControllers/adminCheckAndRemoveDeletedVideos.js
+++ server/controllers/adminControllers/adminPostsControllers/adminCheckAndRemoveDeletedVideos.js
@@ -5,20 +5,15 @@ const axios = require('axios')
 module.exports = async (req,res)=>{
     try {
         res.end()
-        res.setHeader('Content-Type', 'text/plain');
         await postSchema.find({$and:[{type:'video'},{status:'published'}]}).exec().then(async posts=>{
             try {
                 for await (const post of posts){
                     if (post?.videoEmbedCode){
                            await axios.get(post.videoEmbedCode).then(()=>{
-
-                               // res.write(`http://localhost:3000/post/video/${post._id} is ok`);
-
+                               console.log(`http://localhost:3000/post/video/${post._id} is ok`)
                            }).catch(async err=>{
                                 if (err?.response?.status >= 400 && err?.response?.status < 499 || err.code === 'ENOTFOUND'){
                                 await postSchema.findByIdAndUpdate(post._id,{$set:{status:'trash'}},{new:true}).exec().then(trashedpost=>{
-                                    // res.setHeader('Content-Type', 'text/html');
-                                    // res.write(`http://localhost:3000/post/video/${post._id} trashed`);
                                     console.log(`http://localhost:3000/post/video/${post._id} trashed`)
                                     })
                                 }else {
@@ -27,7 +22,6 @@ module.exports = async (req,res)=>{
                             })
                     }
                 }
-
             }catch (err){
                 console.log(err)
             }
