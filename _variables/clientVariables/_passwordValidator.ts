80fead20501b1abca5db8885bfcb896ac01606bd
mlnima <mmlnima@gmail.com>
2021-12-04 15:05:44 +0100

    password validator

--- _variables/clientVariables/_passwordValidator.ts
+++ _variables/clientVariables/_passwordValidator.ts
@@ -2,7 +2,9 @@ const _passwordValidator = (password:string|undefined)=>{
     //return password? (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).test(password):false
 
     //Minimum eight characters, at least one letter and one number
-    return password? (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/).test(password):false
+    // return password? (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/).test(password):false
+    //Minimum eight characters, at least one letter and one number allow special charecter
+     return password? (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{8,}/).test(password):false
 }
 
 export default _passwordValidator
