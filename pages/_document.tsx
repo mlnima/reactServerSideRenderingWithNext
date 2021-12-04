f6d33703f7a8a72681a9785c1dd34f8ab3f5bd51
mlnima <mmlnima@gmail.com>
2021-12-04 13:33:20 +0100

    strict diabled for some client servers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Document, {Html, Head, Main, NextScript,DocumentInitialProps, DocumentContext} from 'next/document'
 import {ServerStyleSheet} from 'styled-components';
 
-// @ts-ignore
 class MyDocument extends Document {
     static async getInitialProps(ctx:DocumentContext) :Promise<DocumentInitialProps> {
         const sheet = new ServerStyleSheet();
