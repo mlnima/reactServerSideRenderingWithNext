5299f0c74a30f22c346d719b2bc894c7fc4d00a2
mlnima <mmlnima@gmail.com>
2022-01-15 23:50:45 +0100

    merge metadata script

--- tools/mergeDuplicateMetaData.js
+++ tools/mergeDuplicateMetaData.js
@@ -17,28 +17,15 @@ const mergeDuplicateMetaData = async (type) => {
                 if (meta?.name !== metaName) {
                     const lowerCasedMeta = await metaSchema.findOne({name: metaName}).exec()
                     if (lowerCasedMeta) {
-                        //console.log({from:meta?._id, to : lowerCasedMeta._id})
-                        //console.log({from: meta?.name, to: lowerCasedMeta.name})
                         console.log(meta?.name)
                         await postSchema.find({[type]: meta?._id}).exec().then(async posts => {
                             if (posts.length){
                                 for await (const post of posts){
                                     await postSchema.findByIdAndUpdate(post?._id,{$pull:{[type]:meta?._id}},{new:true}).exec().then(async updatedPost=>{
-                                        // console.log(updatedPost)
                                         await postSchema.findByIdAndUpdate(post?._id,{$push:{[type]:lowerCasedMeta._id}},{new:true}).exec().then(updatedPost=>{
-                                            // console.log(updatedPost)
                                         })
                                     })
                                 }
-
-                                // const findPost = await postSchema.findById(posts[0]?._id).exec()
-                                // console.log(findPost)
-                                // await postSchema.findByIdAndUpdate(posts[0]?._id,{$pull:{[type]:meta?._id}},{new:true}).exec().then(async updatedPost=>{
-                                //     console.log(updatedPost)
-                                //     await postSchema.findByIdAndUpdate(posts[0]?._id,{$push:{[type]:lowerCasedMeta._id}},{new:true}).exec().then(updatedPost=>{
-                                //         console.log(updatedPost)
-                                //     })
-                                // })
                             }
                         })
                         await metaSchema.findByIdAndDelete(meta?._id).exec()
@@ -49,22 +36,7 @@ const mergeDuplicateMetaData = async (type) => {
         }).catch(err => {
             console.log(err)
         })
-        // console.log('next')
-        //  for await (const duplicates of state.lowerCasesWithDuplicate){
-        //     await postSchema.find({[type]:[duplicates.from]}).exec().then(async posts=>{
-        //               for await (const post of posts){
-        //                  await postSchema.findByIdAndUpdate({_id:post?._id},{$pull:{[type]:duplicates.from},$push:{[type]:duplicates.to}},{new:true}).exec().then(updatedPost=>{
-        //                      console.log(post?._id,duplicates.from,duplicates.to)
-        //                  })
-        //               }
-        //
-        //     })
-        //      await metaSchema.findByIdAndDelete(duplicates.from).exec()
-        //  }
-        // console.log('done')
-        // console.log(state.lowerCasesWithDuplicate)
 
-        //
     } catch (err) {
 
     }
