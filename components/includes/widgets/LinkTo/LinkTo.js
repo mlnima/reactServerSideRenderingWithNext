162133cbd36c2382001506dc2454eeeb6779f05c
mlnima <mmlnima@gmail.com>
2022-02-21 18:53:25 +0100

    linkTo widget link use got a class name

--- components/includes/widgets/LinkTo/LinkTo.js
+++ components/includes/widgets/LinkTo/LinkTo.js
@@ -8,14 +8,14 @@ const LinkTo = props => {
         if (props.linkToType === 'internal') {
             return (
                 <Link href={props.linkTo ? props.linkTo : '/'} as={props.linkToAs ? props.linkToAs : props.linkTo || '/'}>
-                    <a target={props.linkToWindowType ? props.linkToWindowType : '_self'}>
+                    <a target={props.linkToWindowType ? props.linkToWindowType : '_self'} className={'link-to'}>
                     {props.translations ? props.translations[router.locale] ? props.translations[router.locale].linkToText || props.linkToText : props.linkToText : props.linkToText}
                     </a>
                 </Link>
             )
         } else if (props.linkToType === 'external') {
             return (
-                <a href={props.linkTo} target={props.linkToWindowType ? props.linkToWindowType : '_self'}>
+                <a href={props.linkTo} target={props.linkToWindowType ? props.linkToWindowType : '_self'} className={'link-to'}>
                     {props.translations ? props.translations[router.locale] ? props.translations[router.locale].linkToText || props.linkToText : props.linkToText : props.linkToText}
                 </a>
             );
