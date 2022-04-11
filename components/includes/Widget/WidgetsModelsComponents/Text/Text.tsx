c8d9a2e480474f53ba57c740a01b6bb2e8711bb4
mlnima <mmlnima@gmail.com>
2022-04-11 12:43:10 +0200

    widget text updated

--- components/includes/Widget/WidgetsModelsComponents/Text/Text.tsx
+++ components/includes/Widget/WidgetsModelsComponents/Text/Text.tsx
@@ -1,9 +1,9 @@
-// import parse from 'html-react-parser';
+import parse from 'html-react-parser';
 import styled from "styled-components";
 import {useRouter} from "next/router";
-import {FC} from "react";
+import {FC, useState} from "react";
 
-const TextStyledDiv = styled.div`
+const WidgetTextTextDataStyledDiv = styled.div`
   color: var(--main-text-color);
   max-width: 100vw;
 `
@@ -16,18 +16,22 @@ interface TextPropTypes {
 const Text: FC<TextPropTypes> = ({translations, text}) => {
     const {locale} = useRouter();
 
+    const [textToRender,setTextToRender] = useState(()=>{
+        return locale === process.env.NEXT_PUBLIC_DEFAULT_LOCAL ? text : translations?.[locale]?.text || text || ''
+    })
 
     //const textData = translations ? translations[locale] ? translations[locale].text || text : text : text;
-    const textData = locale === process.env.NEXT_PUBLIC_DEFAULT_LOCAL ? text : translations?.[locale]?.text || text || ''
+    // const textData = locale === process.env.NEXT_PUBLIC_DEFAULT_LOCAL ? text : translations?.[locale]?.text || text || ''
 
-    // const data = parse(textData);
+    const data = parse(textToRender);
+
     //remove widgetText className after live project custom styles updated
     return (
-        <TextStyledDiv className='widgetText widget-text'>
-
-            {textData}
-        </TextStyledDiv>
+        <WidgetTextTextDataStyledDiv className='widgetText widget-text'>
+            {data}
+        </WidgetTextTextDataStyledDiv>
     );
+
 };
+
 export default Text;
-// {/*{data}*/}
