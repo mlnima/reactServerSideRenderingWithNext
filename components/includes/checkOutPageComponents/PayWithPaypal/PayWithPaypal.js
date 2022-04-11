3d3db4979266434d07a17eed38170c9284125083
mlnima <mmlnima@gmail.com>
2022-04-11 12:06:53 +0200

    react 18 - testing live production

--- components/includes/checkOutPageComponents/PayWithPaypal/PayWithPaypal.js
+++ components/includes/checkOutPageComponents/PayWithPaypal/PayWithPaypal.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-
-const PayPalButton = paypal.Buttons.driver("react", {React, ReactDOM});
-
-const PayWithPayPal = props => {
-
-    return (
-        <PayPalButton
-            createOrder={(data, actions) =>props.createOrder(data, actions)}
-            onApprove={(data, actions) => props.onApprove(data, actions)}
-            onError={err => props.onErrorHandler(err)}
-        />
-    )
-};
-export default PayWithPayPal;
+// import React from 'react';
+// import ReactDOM from "react-dom";
+//
+// const PayPalButton = paypal?.Buttons?.driver("react", {React, ReactDOM});
+//
+// const PayWithPayPal = props => {
+//
+//     return (
+//         <PayPalButton
+//             createOrder={(data, actions) =>props.createOrder(data, actions)}
+//             onApprove={(data, actions) => props.onApprove(data, actions)}
+//             onError={err => props.onErrorHandler(err)}
+//         />
+//     )
+// };
+// export default PayWithPayPal;
