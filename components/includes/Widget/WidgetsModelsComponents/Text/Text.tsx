442ccacccf8b13dc322a11dc83f8e10cce308975
mlnima <mmlnima@gmail.com>
2022-04-11 15:48:03 +0200

    widget text updated

--- components/includes/Widget/WidgetsModelsComponents/Text/Text.tsx
+++ components/includes/Widget/WidgetsModelsComponents/Text/Text.tsx
@@ -18,7 +18,7 @@ const Text: FC<TextPropTypes> = ({translations, text}) => {
     const {locale} = useRouter();
 
     const textToRender = useMemo(() => {
-        return parse((locale === process.env.NEXT_PUBLIC_DEFAULT_LOCAL ? text : translations?.[locale]?.text || text || '').replaceAll('\n','').replaceAll('  ',' '), { trim: true });
+        return parse((locale === process.env.NEXT_PUBLIC_DEFAULT_LOCAL ? text : translations?.[locale]?.text || text || '').replaceAll('\n','').replace(/  +/g, ' '), { trim: true });
     }, [])
 
     return (
