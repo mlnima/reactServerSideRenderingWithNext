93f8e62d7fcd4acc9cec2f3132e3e07d69e00cd5
mlnima <mmlnima@gmail.com>
2022-05-04 03:48:34 +0200

    packages - admin panel widget editors -post page SEO

--- store/clientReducers/widgetsReducer.ts
+++ store/clientReducers/widgetsReducer.ts
@@ -1,23 +1,12 @@
 import {HYDRATE} from 'next-redux-wrapper';
 import _reduceWidgetsToGroups from "../../_variables/_reduceWidgetsToGroups/_reduceWidgetsToGroups";
 import {SET_REQUESTED_WIDGETS, SET_WIDGETS, SET_WIDGETS_IN_GROUPS} from "@store/types";
-// import {findIndex} from 'lodash'
-import {
-    DELETE_WIDGET,
-    UPDATE_WIDGET
-} from "@store/adminTypes";
-import {object} from "prop-types";
 
 const initialState = {
     widgetInGroups:{},
     requestedWidgets:[]
 }
 
-// ...action?.payload?.widgets || [],
-// widgetInGroups:{
-//     ...state.widgetInGroups,
-//     // ...action?.payload?.widgetInGroups
-// }
 export const widgetsReducer = (state = initialState, action) => {
 
     switch (action.type) {
@@ -47,19 +36,6 @@ export const widgetsReducer = (state = initialState, action) => {
                 ...state,
                 requestedWidgets: [...new Set([...state.requestedWidgets,...action.payload])]
             }
-
-        // case  SET_WIDGETS_FOR_ADMIN:
-        //     return{
-        //        ...state,
-        //         widgets : action?.payload
-        //     }
-        // case SAVE_NEW_WIDGET:
-        //
-        //     return {
-        //         ...state,
-        //         widgets: [...state.widgets, action.payload]
-        //     };
-
         default:
             return state
     }
