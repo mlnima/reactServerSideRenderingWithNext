f8a93b754258ddb1f5ee46b7113f7d2235e22b3a
mlnima <mmlnima@gmail.com>
2022-10-25 19:52:47 +0200

    before changes for nextJs13

--- expressServer/controllers/clientControllers/clientFileManagerControllers/ugc_postImagesUpload.ts
+++ expressServer/controllers/clientControllers/clientFileManagerControllers/ugc_postImagesUpload.ts
@@ -2,8 +2,56 @@ import fsExtra from 'fs-extra'
 import sharp from 'sharp'
 import fs from 'fs'
 import postSchema from "../../../models/postSchema";
+import {Worker, isMainThread,parentPort} from 'worker_threads';
+import path from "path";
+
+
+// const runWorker = async (postId,imageIndex)=>{
+//     if (isMainThread){
+//         const workerPath = path.join(__dirname,'../../../workers/clientWorkers/clientUploadPostImageWorker/worker.js') ;
+//         const worker = new Worker(
+//             workerPath,
+//             {
+//                 workerData:{
+//                     postId,
+//                     imageIndex,
+//                 }
+//             }
+//         )
+//
+//        await worker.once('message',(result) =>{
+//             worker.postMessage({ exit: true })
+//             return result
+//         })
+//
+//         worker.on('error', error => {
+//             console.log('error:',error);
+//         });
+//
+//         worker.on('exit', exitCode => {
+//             console.log('exitCode : ',exitCode);
+//
+//         })
+//     }else{
+//
+//         parentPort.on("message", (commandFromMainThread) => {
+//             if (commandFromMainThread.exit) {
+//                 console.log('terminating thread')
+//                 process.exit(0);
+//             }
+//         });
+//     }
+// }
+
+
+
+
 
 const ugc_postImagesUpload = async (req, res) => {
+
+
+
+
     try {
         const postId = req.body._id;
         let imageIndex = req.body.imageIndex;
@@ -11,34 +59,64 @@ const ugc_postImagesUpload = async (req, res) => {
         await fsExtra.ensureDir(directoryPath)
         const images = req.files;
         const imagesKeys = Object.keys(images)
-        let uploadedImagesUrls = []
+
+        const image = images[0]
 
         for await (const image of imagesKeys) {
-
             const tempPath = `./public/uploads/posts/${postId}/temp`
             const filePath = `./public/uploads/posts/${postId}/${imageIndex}.${'webp'}`
-
             await images[image].mv(tempPath)
-            await sharp(tempPath).webp({lossless: true})
-                .resize({width: 640, height: 360, fit: sharp.fit.contain})
-                .toFile(filePath)
-                .then(() => {
-                    try {
-                        fs.unlinkSync(tempPath)
-                        uploadedImagesUrls = [...uploadedImagesUrls, filePath.replace('./', '/')]
 
-                    } catch (error) {
-                        console.log("tempPath remove file", error)
+            if (isMainThread){
+                const workerPath = path.join(__dirname,'../../../workers/clientWorkers/clientUploadPostImageWorker/worker.js') ;
+                const worker = new Worker(
+                    workerPath,
+                    {
+                        workerData:{
+                            postId,
+                            imageIndex,
+                        }
                     }
+                )
+
+                 worker.once('message',async (result) =>{
+                    worker.postMessage({ exit: true })
+                    //  let uploadedImagesUrls = [result.response]
+                    // uploadedImagesUrls = [...uploadedImagesUrls,result.response]
+                     await postSchema.findByIdAndUpdate(postId, {$addToSet: {images: {$each: [result.response]}}}, {new: true}).exec().then(async updated => {
+                         res.json({message: 'uploaded', images: updated.images})
+                     }).catch(err => {
+                         console.log(err)
+                         res.json({message: 'Something Went Wrong'})
+                     })
                 })
+
+                //  worker.on('message',(result) =>{
+                //     // worker.postMessage({ exit: true })
+                //     console.log('on:',result)
+                //     // uploadedImagesUrls = [...uploadedImagesUrls,result.response]
+                // })
+                worker.on('error', error => {
+                    console.log('error:',error);
+                });
+
+                worker.on('exit', exitCode => {
+                    console.log('exitCode : ',exitCode);
+
+                })
+            }else{
+
+                parentPort.on("message", (commandFromMainThread) => {
+                    if (commandFromMainThread.exit) {
+                        console.log('terminating thread')
+                        process.exit(0);
+                    }
+                });
+            }
+
         }
 
-        await postSchema.findByIdAndUpdate(postId, {$addToSet: {images: {$each: uploadedImagesUrls}}}, {new: true}).exec().then(async updated => {
-            res.json({message: 'uploaded', images: updated.images})
-        }).catch(err => {
-            console.log(err)
-            res.json({message: 'Something Went Wrong'})
-        })
+
 
     } catch (error) {
         console.log("ugc_postImagesUpload", error)
@@ -49,3 +127,46 @@ const ugc_postImagesUpload = async (req, res) => {
 }
 
 export default ugc_postImagesUpload;
+
+
+
+// try {
+//     const postId = req.body._id;
+//     let imageIndex = req.body.imageIndex;
+//     const directoryPath = './public/uploads/posts/' + postId + '/';
+//     await fsExtra.ensureDir(directoryPath)
+//     const images = req.files;
+//     const imagesKeys = Object.keys(images)
+//     let uploadedImagesUrls = []
+//
+//     for await (const image of imagesKeys) {
+//
+//         const tempPath = `./public/uploads/posts/${postId}/temp`
+//         const filePath = `./public/uploads/posts/${postId}/${imageIndex}.${'webp'}`
+//
+//         await images[image].mv(tempPath)
+//         await sharp(tempPath).webp({lossless: true})
+//             .resize({width: 640, height: 360, fit: sharp.fit.contain})
+//             .toFile(filePath)
+//             .then(() => {
+//                 try {
+//                     fs.unlinkSync(tempPath)
+//                     uploadedImagesUrls = [...uploadedImagesUrls, filePath.replace('./', '/')]
+//
+//                 } catch (error) {
+//                     console.log("tempPath remove file", error)
+//                 }
+//             })
+//     }
+//
+//     await postSchema.findByIdAndUpdate(postId, {$addToSet: {images: {$each: uploadedImagesUrls}}}, {new: true}).exec().then(async updated => {
+//         res.json({message: 'uploaded', images: updated.images})
+//     }).catch(err => {
+//         console.log(err)
+//         res.json({message: 'Something Went Wrong'})
+//     })
+//
+// } catch (error) {
+//     console.log("ugc_postImagesUpload", error)
+//     res.end()
+// }
--- expressServer/workers/clientWorkers/clientUploadPostImageWorker/worker.ts
+++ expressServer/workers/clientWorkers/clientUploadPostImageWorker/worker.ts
@@ -1,23 +1,41 @@
 import dotenv from 'dotenv';
 import {parentPort, workerData} from 'worker_threads';
-import postSchema from '../../../models/postSchema';
 import connectToDatabase from '../../../_variables/connectToDatabase';
-import mongoose from 'mongoose';
+import sharp from 'sharp'
+import fs from 'fs'
+import path from "path";
 
 dotenv.config();
-connectToDatabase('setMetaThumbnailsAndCount :').finally();
-
+connectToDatabase('client upload post image :').finally();
 
 const worker = async (workerData) => {
     try {
+        const tempPath = path.join(__dirname, `../../../../public/uploads/posts/${workerData.postId}/temp`)
+        const targetPath = path.join(__dirname, `../../../../public/uploads/posts/${workerData.postId}/${workerData.imageIndex}.webp`)
 
-    }catch (error){
+        return await sharp(tempPath).webp({lossless: true})
+            .resize({width: 640, height: 360, fit: sharp.fit.contain})
+            .toFile(targetPath)
+            .then(async () => {
+                try {
+                    // await fs.chmodSync(tempPath, 777)
+                    await fs.unlinkSync(tempPath)
 
+
+                } catch (error) {
+                    console.log("tempPath remove file", error)
+                }
+                return {response: `/public/uploads/posts/${workerData.postId}/${workerData.imageIndex}.webp`}
+            })
+    } catch (error) {
+        console.log(error)
+        // return null
     }
 }
 
 
-worker(workerData).then(res => {
-    parentPort.postMessage(res)
-    process.exit(0);
+worker(workerData).then(result => {
+    console.log('worker result:',result)
+    parentPort.postMessage(result)
+    process.exit()
 })
