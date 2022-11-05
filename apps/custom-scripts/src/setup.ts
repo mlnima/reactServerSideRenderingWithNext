27f954042a896e4720951cc8da7d56e1a04e31ae
mlnima <mmlnima@gmail.com>
2022-11-05 17:40:47 +0100

    setup script

--- apps/custom-scripts/src/setup.ts
+++ apps/custom-scripts/src/setup.ts
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
+
 dotenv.config({path: '../../.env'});
 import {connectToDatabase} from 'custom-server-util';
+
 connectToDatabase('Setup').finally()
-import {settingSchema,widgetSchema,userSchema} from 'models';
+import {settingSchema, widgetSchema, userSchema} from 'models';
 import bcrypt from 'bcryptjs';
 import uuidAPIKey from 'uuid-apikey';
 import defaultIdentitySettings from "../asset/defaultIdentitySettings";
@@ -10,18 +12,24 @@ import defaultDesignSettings from "../asset/defaultDesignSettings";
 import defaultAdminAccountData from "../asset/defaultAdminAccountData";
 import defaultWidgets from "../asset/defaultWidgets";
 
-const runScripts = async ()=>{
 
+const setWidgets = async () => {
     try {
-
-        for await (const widget of defaultWidgets){
+        for await (const widget of defaultWidgets) {
             const dataToSave = new widgetSchema(widget)
             dataToSave.save().catch(err => {
                 console.log(err)
                 console.log('Error on widget set')
             })
         }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+
+const setSettings = async () => {
+    try {
         const identityToSave = new settingSchema(defaultIdentitySettings)
 
         const siteDesignToSave = new settingSchema(defaultDesignSettings)
@@ -29,9 +37,15 @@ const runScripts = async ()=>{
         await identityToSave.save(error => console.log(error))
 
         await siteDesignToSave.save(error => console.log(error))
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+
+const createAdminAccount = async () => {
+    try {
         const APIKey = await uuidAPIKey.create()
-
         await bcrypt.hash(defaultAdminAccountData.password, 10, function (err, hash) {
             if (err) {
                 console.log(err)
@@ -48,13 +62,18 @@ const runScripts = async ()=>{
             }
         });
 
-
-    }catch (error){
+    } catch (error) {
         console.log(error)
     }
-
 }
 
-runScripts().then(()=>process.exit())
+
+const runScripts = async () => {
+    await setWidgets()
+    await setSettings()
+    await createAdminAccount
+}
+
+runScripts().then(() => process.exit())
 
 
