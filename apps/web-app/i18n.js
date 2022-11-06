c32f3ef5095ad60d189be3d8fba6b1dd4b3de7d8
mlnima <mmlnima@gmail.com>
2022-11-06 19:05:22 +0100

    multi domains for multi locals

--- apps/web-app/i18n.js
+++ apps/web-app/i18n.js
@@ -1,13 +1,25 @@
-module.exports =  {
-    locales: process.env.NEXT_PUBLIC_LOCALS?.split(' ') || [],
-    defaultLocale: process.env.NEXT_PUBLIC_DEFAULT_LOCAL,
-    logger:()=>null,
+const locales = process.env.NEXT_PUBLIC_LOCALS?.split(' ') || [];
+const defaultLocale = process.env.NEXT_PUBLIC_DEFAULT_LOCAL;
+const domains = !process.env.NEXT_PUBLIC_DOMAINS ? {} :
+    process.env.NEXT_PUBLIC_DOMAINS.split(' ').map((domain, index) => {
+        return {
+            domain,
+            defaultLocale: locales[index],
+        }
+    })
+
+
+module.exports = {
+    locales,
+    defaultLocale,
+    ...domains,
+    logger: () => null,
     logBuild: false,
     localeDetection: false,
     // loadLocaleFrom: (lang, ns) =>
     //     import(`./public/locales/${lang}/${ns}.json`).then((m) => m.default),
     pages: {
-        "*": ['common','customTranslation'],
+        "*": ['common', 'customTranslation'],
         // "/": ["home", "example"],
         "/profile": ["profile"],
         "/profile/edit": ["profile"]
