a5ee9aa5beef55e0798a07da3e08e5e8ea30544c
Nima Malayeri <mmlnima@gmail.com>
2023-01-09 21:25:41 +0100

    app layout optimized

--- apps/web-app/store_toolkit/hooks.ts
+++ apps/web-app/store_toolkit/hooks.ts
@@ -1,10 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
-import type { RootAdminState, AdminDispatch } from '../../../trash/oldAdminPagesAndComponents/store/adminStore'
 
-// Use throughout your appex instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
-export const useAdminDispatch = () => useDispatch<AdminDispatch>()
-export const useAdminSelector: TypedUseSelectorHook<RootAdminState> = useSelector
