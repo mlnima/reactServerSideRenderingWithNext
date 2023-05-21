664375c1131c97a7a067a829cc9d9f201cb710f4
Nima Malayeri <mmlnima@gmail.com>
2023-05-22 01:14:41 +0200

    SMTP server send test email script

--- apps/mail-server/src/tests/outGoingEmailFromOurServer.js
+++ apps/mail-server/src/tests/outGoingEmailFromOurServer.js
@@ -1,17 +1,17 @@
+require('dotenv').config({path: '../../../../.env'})
+
 const nodemailer = require('nodemailer');
+
+console.log('console=> ',process.env.JWT_KEY)
 
 const transporter = nodemailer.createTransport({
     host: 'mail.trdland.de',
-    port: 587,
-    secure: false,
-    requireTLS: true,
+    port: 465,
+    secure: true, // for port 465
     auth: {
-        user: 'no-reply',
-        pass: 'trdland$%',
+        user: 'welcome',
+        pass: process.env.JWT_KEY,
     },
-    tls: {
-        rejectUnauthorized: false
-    }
 });
 
 const mailOptions = {
@@ -28,3 +28,45 @@ transporter.sendMail(mailOptions, (error, info) => {
         console.log('Email sent:', info.response);
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// const nodemailer = require('nodemailer');
+//
+// const transporter = nodemailer.createTransport({
+//     host: 'mail.trdland.de',
+//     port: 465,
+//     secure: false,
+//     // requireTLS: true,
+//     auth: {
+//         user: 'welcome',
+//         pass: 'trdland$%',
+//     },
+//     // tls: {
+//     //     rejectUnauthorized: false
+//     // }
+// });
+//
+// const mailOptions = {
+//     from: 'welcome@trdland.de',
+//     to: 'mmlnima@gmail.com',
+//     subject: 'Welcome to MyApp!',
+//     text: 'Thank you for registering on MyApp.',
+// };
+//
+// transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//         console.error('Error sending email:', error);
+//     } else {
+//         console.log('Email sent:', info.response);
+//     }
+// });
