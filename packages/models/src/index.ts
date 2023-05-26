2be33594cbfc96cf7e973e216a329fa72136f57e
Nima Malayeri <mmlnima@gmail.com>
2023-05-26 12:20:47 +0200

    schema package export bug

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,42 +1,62 @@
-import settingSchema from "./settingSchema";
-import chatroomSchema from "./chatroomSchema";
-import commentSchema from "./commentSchema";
-import contactFromSchema from "./contactFromSchema";
-import conversationSchema from "./conversationSchema";
-import formSchema from "./formSchema";
-import metaSchema from "./metaSchema";
-import orderSchema from "./orderSchema";
-import pageSchema from "./pageSchema";
-import postSchema from "./postSchema";
-import searchKeywordSchema from "./searchKeywordSchema";
-import translateSchema from "./translateSchema";
-import userSchema from "./userSchema";
-import widgetSchema from "./widgetSchema";
-import chatroomMessageSchema from "./chatroomMessageSchema";
-import fileSchema from "./fileSchema";
-import messengerConversationMessageSchema from "./messengerConversationMessageSchema";
-import messengerConversationSchema from "./messengerConversationSchema";
-import emailSchema from "./emailSchema";
+export { default as settingSchema} from './settingSchema';
+export { default as chatroomSchema} from './chatroomSchema';
+export { default as commentSchema} from './commentSchema';
+export { default as contactFromSchema} from './contactFromSchema';
+export { default as conversationSchema} from './conversationSchema';
+export { default as formSchema} from './formSchema';
+export { default as metaSchema} from './metaSchema';
+export { default as orderSchema} from './orderSchema';
+export { default as pageSchema} from './pageSchema';
+export { default as postSchema} from './postSchema';
+export { default as searchKeywordSchema} from './searchKeywordSchema';
+export { default as translateSchema} from './translateSchema';
+export { default as userSchema} from './userSchema';
+export { default as widgetSchema} from './widgetSchema';
+export { default as chatroomMessageSchema} from './chatroomMessageSchema';
+export { default as fileSchema} from './fileSchema';
+export { default as messengerConversationMessageSchema} from './messengerConversationMessageSchema';
+export { default as messengerConversationSchema} from './messengerConversationSchema';
+export { default as emailSchema} from './emailSchema';
 
+// import settingSchema from "./settingSchema";
+// import chatroomSchema from "./chatroomSchema";
+// import commentSchema from "./commentSchema";
+// import contactFromSchema from "./contactFromSchema";
+// import conversationSchema from "./conversationSchema";
+// import formSchema from "./formSchema";
+// import metaSchema from "./metaSchema";
+// import orderSchema from "./orderSchema";
+// import pageSchema from "./pageSchema";
+// import postSchema from "./postSchema";
+// import searchKeywordSchema from "./searchKeywordSchema";
+// import translateSchema from "./translateSchema";
+// import userSchema from "./userSchema";
+// import widgetSchema from "./widgetSchema";
+// import chatroomMessageSchema from "./chatroomMessageSchema";
+// import fileSchema from "./fileSchema";
+// import messengerConversationMessageSchema from "./messengerConversationMessageSchema";
+// import messengerConversationSchema from "./messengerConversationSchema";
+// import emailSchema from "./emailSchema";
 
-export {
-    messengerConversationSchema,
-    messengerConversationMessageSchema,
-    settingSchema,
-    fileSchema,
-    chatroomMessageSchema,
-    chatroomSchema,
-    commentSchema,
-    contactFromSchema,
-    conversationSchema,
-    formSchema,
-    metaSchema,
-    orderSchema,
-    pageSchema,
-    postSchema,
-    searchKeywordSchema,
-    translateSchema,
-    userSchema,
-    emailSchema,
-    widgetSchema
-}
+//
+// export {
+//     messengerConversationSchema,
+//     messengerConversationMessageSchema,
+//     settingSchema,
+//     fileSchema,
+//     chatroomMessageSchema,
+//     chatroomSchema,
+//     commentSchema,
+//     contactFromSchema,
+//     conversationSchema,
+//     formSchema,
+//     metaSchema,
+//     orderSchema,
+//     pageSchema,
+//     postSchema,
+//     searchKeywordSchema,
+//     translateSchema,
+//     userSchema,
+//     emailSchema,
+//     widgetSchema
+// }
