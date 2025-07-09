dd8bffc66dceafa7d2d3ac1f7a9f9819b6e56df6
Nima M <mmlnima@gmail.com>
2025-07-09 10:55:24 +0200

    nginx access monitor widget for dashboard

--- apps/web-application/app/admin/page.tsx
+++ apps/web-application/app/admin/page.tsx
@@ -1,8 +1,10 @@
 import MonitorResources from '@components/dashboardWidgets/MonitorResources/MonitorResources';
+import NginxLogMonitor from '@components/dashboardWidgets/NginxLogMonitor/NginxLogMonitor';
 
 export default function Page() {
   console.log(process.memoryUsage());
   return <div>
     <MonitorResources interval={5000} />
+    <NginxLogMonitor/>
   </div>;
 }
